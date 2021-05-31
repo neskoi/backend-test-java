9e50b3cb5a23da04918422bc5bce7e89862f3816
Koi Nes <jan.nes@fatec.sp.gov.br>
2021-05-31 11:06:57 -0300

    chore: configuração de dependencias.

---
+++ src/main/java/com/fcamara/minhaVaga/MinhaVagaApplication.java
@@ -0,0 +1,13 @@
+package com.fcamara.minhaVaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaVagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaVagaApplication.class, args);
+	}
+
+}
