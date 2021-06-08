cb7eb21cec4869ed552701223ac67f65de88a0b7
Koi Nes <jan.nes@fatec.sp.gov.br>
2021-06-08 10:39:59 -0300

    refactor: modificação no data.sql, inserção de alguns dados iniciais.

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -20,58 +20,5 @@ INSERT INTO car_park(name, password, cnpj, email, phone)
 VALUES('Patio não Policial', '$2a$10$ENa9esVD8dBOzWKegTaHY.IsucQ.m29dXKhSoyJYgGDd1rCNFqcka', '74713858000130', 'pp@com.com', '3362240144');
 
 /*===========================================================*/
-INSERT INTO state(short_name, name) 
-VALUES('AC', 'Acre');
-INSERT INTO state(short_name, name) 
-VALUES('AL', 'Alagoas');
-INSERT INTO state(short_name, name) 
-VALUES('AP', 'Amapá');
-INSERT INTO state(short_name, name) 
-VALUES('AM', 'Amazonas');
-INSERT INTO state(short_name, name) 
-VALUES('BA', 'Bahia');
-INSERT INTO state(short_name, name) 
-VALUES('CE', 'Ceará');
-INSERT INTO state(short_name, name) 
-VALUES('DF', 'Distrito Federal');
-INSERT INTO state(short_name, name) 
-VALUES('ES', 'Espírito Santo');
-INSERT INTO state(short_name, name) 
-VALUES('GO', 'Goiás');
-INSERT INTO state(short_name, name) 
-VALUES('MA', 'Maranhão');
-INSERT INTO state(short_name, name) 
-VALUES('MT', 'Mato Grosso');
-INSERT INTO state(short_name, name) 
-VALUES('MS', 'Mato Grosso do Sul');
-INSERT INTO state(short_name, name) 
-VALUES('MG', 'Minas Gerais');
-INSERT INTO state(short_name, name) 
-VALUES('PA', 'Pará');
-INSERT INTO state(short_name, name) 
-VALUES('PB', 'Paraíba');
-INSERT INTO state(short_name, name) 
-VALUES('PR', 'Paraná');
-INSERT INTO state(short_name, name) 
-VALUES('PE', 'Pernambuco');
-INSERT INTO state(short_name, name) 
-VALUES('PI', 'Piauí');
-INSERT INTO state(short_name, name) 
-VALUES('RJ', 'Rio de Janeiro');
-INSERT INTO state(short_name, name) 
-VALUES('RN', 'Rio Grande do Norte');
-INSERT INTO state(short_name, name) 
-VALUES('RS', 'Rio Grande do Sul');
-INSERT INTO state(short_name, name) 
-VALUES('RO', 'Rondônia');
-INSERT INTO state(short_name, name) 
-VALUES('RR', 'Roraima');
-INSERT INTO state(short_name, name) 
-VALUES('SC', 'Santa Catarina');
-INSERT INTO state(short_name, name) 
-VALUES('SP', 'São Paulo');
-INSERT INTO state(short_name, name) 
-VALUES('SE', 'Sergipe');
-INSERT INTO state(short_name, name) 
-VALUES('TO','Tocantins');
+
 /*===========================================================*/
